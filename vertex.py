78b1d05beaf6e33d2af25b6545c4311a5d74dafb
modderme123 <modderme123@gmail.com>
2023-04-01 23:02:56 -0400

    fix pythonta on vertex

--- vertex.py
+++ vertex.py
@@ -1,3 +1,6 @@
+"""
+This module includes our Vertex class. It supports a simple clamping and velocity
+"""
 import random
 
 
@@ -11,15 +14,17 @@ class Vertex:
     vy: float
     pinned: bool
 
-    def __init__(self, x, y):
+    def __init__(self, x: float, y: float) -> None:
         self.mass = 5
+        # We use the 1 - random.random() trick to ensure 0 is not a possible value
+        # This is because random.random() returns a vlue in the range [0, 1)
         self.x = x + ((1 - random.random()) * 0.02 - 0.01)
         self.y = y + ((1 - random.random()) * 0.02 - 0.01)
         self.vx = 0
         self.vy = 0
         self.pinned = False
 
-    def update(self, friction: float, gravity: float, dt: float):
+    def update(self, friction: float, gravity: float, dt: float) -> None:
         """Update self using graph friction, graph gravity, and time change."""
         self.vx *= 1 - (friction * dt)
         self.vy *= 1 - (friction * dt)
